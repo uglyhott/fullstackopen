bbaf7285b2901c3482acc1f6991536e48681899f
Liam Blumenstein-Tait <liam.bt@gmail.com>
2024-05-27 21:42:48 +0200

    add exercise 1.3

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # fullstackopen
 
+My submissions for the exercises of the [Full Stack Open](https://fullstackopen.com/) course.
+
 - [part0](/part0): mermaid.js sequence diagrams
+- [part1](/part1/): Introduction to React
---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,49 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Content = (props) => {
+  console.log("Content: " + props)
+  return (
+    <div>
+      <Part name={props.part1.name} exercises={props.part1.exercises} />
+      <Part name={props.part2.name} exercises={props.part2.exercises} />
+      <Part name={props.part3.name} exercises={props.part3.exercises} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  console.log("Part: " + props)
+  return <p>{props.name} {props.exercises}</p>
+}
+
+const Total = (props) => {
+  return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+    </div>
+  )
+}
+
+export default App
