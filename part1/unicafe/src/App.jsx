333a01d97cf7cae2af611d06b61dcbf5a3dd5171
Liam Blumenstein-Tait <liam.bt@gmail.com>
2024-06-03 15:21:53 +0200

    1.9: unicafe step 4 - step 3 completed previously

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,21 +5,25 @@ const Button = ({ handleClick, text }) => {
 }
 
 const Statistics = ({ good, neutral, bad }) => {
-  const startState = good == 0 && neutral == 0 && bad == 0
+  let startState = good == 0 && neutral == 0 && bad == 0
   const totalCollectedFeedback = good + neutral + bad
   const averageScore = (good + (bad * -1)) / totalCollectedFeedback
   const percentPositive = (good / totalCollectedFeedback) * 100
 
-  return (
-    <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {totalCollectedFeedback}</p>
-      <p>average {startState ? 0 : averageScore}</p>
-      <p>positive {startState ? 0 : percentPositive}</p>
-    </div>
-  )
+  if (startState) {
+    return <p>No feedback given</p>
+  } else {
+    return (
+      <>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {totalCollectedFeedback}</p>
+        <p>average {averageScore}</p>
+        <p>positive {percentPositive}</p>
+      </>
+    )
+  }
 }
 
 const App = () => {
